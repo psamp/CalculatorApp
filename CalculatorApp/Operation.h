300e78102311f60dbe0da630d23c46b67ff65a77
Princess Sampson <ps@kingonomics.com>
2016-08-27 10:23:59 -0400

    Adjust multiplier for sign and 2nd operand labels

---
+++ CalculatorApp/Operation.h
@@ -0,0 +1,13 @@
+//
+//  Operation.h
+//  CalculatorApp
+//
+//  Created by Princess Sampson on 8/27/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Operation : NSObject
+
+@end
---
+++ CalculatorApp/Operation.m
@@ -0,0 +1,13 @@
+//
+//  Operation.m
+//  CalculatorApp
+//
+//  Created by Princess Sampson on 8/27/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import "Operation.h"
+
+@implementation Operation
+
+@end
